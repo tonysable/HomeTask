337e7e5237e4e7a762d671dc49369b9769ed5e74
Sable <bandidos_150@mail.ru>
2022-10-07 00:02:35 +0300

    Задача 13

---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿// int a = 1;
+// while(a <= 10){
+//     Console.WriteLine(a);
+//     a += 1;
+// }
+string str;
+long a;
+Console.WriteLine("Введите число:");
+str = Console.ReadLine();
+a = Convert.ToInt32(str);
+if(a >= 100){
+
+
+    long b = 10;
+    while(a > b){
+        b *= 10;
+    }
+
+    Console.WriteLine(a * 1000 / b % 10);
+}
+else{
+    Console.WriteLine("Нет третьего числа");
+}
