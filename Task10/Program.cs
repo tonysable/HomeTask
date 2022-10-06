51ee86162e2a948f03dbe0d9c5c3e4ba4b658a8d
Sable <bandidos_150@mail.ru>
2022-10-06 22:02:33 +0300

    Задача 10

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿string str;
+int i;
+Console.WriteLine("Введите трёхзначное число:");
+str = Console.ReadLine();
+i = Convert.ToInt32(str);
+int division1 = i / 10;
+int division2 = division1 % 10;
+Console.Write(division2);
+Console.WriteLine(" - вторая цифра числа");
