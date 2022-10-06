94822a2482048b7fe76f9a44a8f7f9c2f8e6a1a4
Sable <bandidos_150@mail.ru>
2022-10-07 00:09:30 +0300

    Задача 15, хоть и под номером 16

---
+++ Task16/Program.cs
@@ -0,0 +1,14 @@
+﻿string str;
+int a;
+Console.WriteLine("Введите число от 1 до 7:");
+str = Console.ReadLine();
+a = Convert.ToInt32(str);
+if(a <= 5 && a >= 1){
+    Console.WriteLine("Будний день");
+}
+else if(a >= 6 && a < 8){
+    Console.WriteLine("Выходной день");
+}
+else{
+    Console.WriteLine("Нет такого дня недели!");
+}
